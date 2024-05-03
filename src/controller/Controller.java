369b917d606a6d47375761841fb97d1b4bfec379
ViAarisKea <viaa0001@stud.kea.dk>
2024-05-03 11:00:02 +0200

    add new member method

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,9 +3,14 @@ package controller;
 import domain_model.Delfin;
 import domain_model.Member;
 
+import java.io.FileNotFoundException;
+
 public class Controller {
-private Delfin delfin;
+    private Delfin delfin = new Delfin();
 
 
+    public boolean addMember(String name, int age, String membership, String discipline) throws FileNotFoundException {
+        return delfin.addMember(name, age, membership, discipline);
+    }
 
 }
