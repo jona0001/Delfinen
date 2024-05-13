4020a95b4296957027ed74118ab7edb72f76ef95
ViAarisKea <viaa0001@stud.kea.dk>
2024-05-13 13:13:10 +0200

    addCompetingMembers

--- src/domain_model/Team.java
+++ src/domain_model/Team.java
@@ -5,4 +5,15 @@ import java.util.ArrayList;
 public class Team {
     private ArrayList<CompetingMember> competingMembers = new ArrayList<>();
     private Trainer trainer;
+    private Discipline discipline;
+
+
+    public Team(Trainer trainer, Discipline discipline) {
+        this.trainer = trainer;
+        this.discipline = discipline;
+    }
+
+    public void addCompetingMembers(CompetingMember member){
+        competingMembers.add(member);
+    }
 }
